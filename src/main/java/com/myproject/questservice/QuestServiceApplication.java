7fbd5f24cd3209342cbe3b88adcde8f05cd01782
Vasilii <vasilytolkachew@gmail.com>
2026-07-11 14:00:28 +0700

    Initiate project structure

---
+++ src/main/java/com/myproject/questservice/QuestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.questservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/questservice/QuestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.questservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
